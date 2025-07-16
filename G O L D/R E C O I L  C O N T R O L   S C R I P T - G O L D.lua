5b86fea0dbeb8867f138b90a8f835fd5fc66ed5f
Yuri Ribeiro <39933686+YuriRibeiro469@users.noreply.github.com>
2025-07-16 17:58:56 -0300

    Update R E C O I L  C O N T R O L   S C R I P T - G O L D.lua

--- G O L D/R E C O I L  C O N T R O L   S C R I P T - G O L D.lua
+++ G O L D/R E C O I L  C O N T R O L   S C R I P T - G O L D.lua
@@ -2,46 +2,21 @@
 
 EnableRCS = true -- False - Disabled True - Enabled
 
-RecoilControlMode = "1" -- Modes: "1", "2", "3", "4", "5", "0 - custom"
-
-RcCustomStrengthX = 2 -- Horizontal force (left-right) (If you don't want it to go left or right, set to 0)
-
-RcCustomStrengthY = 7 -- Vertical force (downwards)
-
-HorizontalDirection = "Left" -- Options: "Left", "Right"
+currentMode = "5" -- Modes: "1", "2", "3", "4", "5", "0 - custom"
 
 RequireToggle = true -- Set to false if it should always be active.
 
 ToggleKey = "CapsLock" -- Toggle keys: "CapsLock", "NumLock", "ScrollLock"
 
-DelayRate = 7 -- Time between changes in milliseconds
 
-
--- Saved Settings --
-if RecoilControlMode == "1" then
-    RcCustomStrengthX = 1
-    RcCustomStrengthY = 8
-    HorizontalDirection = "Right" 
-elseif RecoilControlMode == "2" then
-    RcCustomStrengthX = 11.5
-    RcCustomStrengthY = 11.5
-    HorizontalDirection = "Right" 
-elseif RecoilControlMode == "3" then
-    RcCustomStrengthX = 3.0
-    RcCustomStrengthY = 3.0
-    HorizontalDirection = "Left"
-elseif RecoilControlMode == "4" then
-    RcCustomStrengthX = 12.0
-    RcCustomStrengthY = 12.0
-    HorizontalDirection = "Right" 
-elseif RecoilControlMode == "5" then
-    RcCustomStrengthX = 31.5
-    RcCustomStrengthY = 31.5
-    HorizontalDirection = "Left"
-elseif RecoilControlMode == "0" then
-    -- Custom settings already defined above --
-end
-
+recoil_configs = {
+    ["1"] = { StrengthX = 1,    StrengthY = 8,    Delay = 7 },
+    ["2"] = { StrengthX = 11.5, StrengthY = 11.5, Delay = 7 },
+    ["3"] = { StrengthX = -3.0, StrengthY = 3.0,  Delay = 7 },
+    ["4"] = { StrengthX = 12.0, StrengthY = 12.0, Delay = 7 },
+    ["5"] = { StrengthX = -31.5,StrengthY = 31.5, Delay = 7 },
+    ["0"] = { StrengthX = -2,   StrengthY = 7,    Delay = 7 }
+}
 
 -- Script --
 EnablePrimaryMouseButtonEvents(true)
@@ -59,27 +34,24 @@ function OnEvent(event, arg)
 end
 
 function handleRecoil()
-    if IsMouseButtonPressed(3) then -- Right mouse button to aim
+    local config = recoil_configs[currentMode]
+    if not config then
+        return
+    end
+
+    if IsMouseButtonPressed(3) then  -- Right mouse button to aim
         repeat
             if IsMouseButtonPressed(1) then -- Left mouse button to shoot
                 repeat
-                    local moveX = calculateHorizontalRecoil()
-                    local moveY = RcCustomStrengthY
+                    local moveX = config.StrengthX
+                    local moveY = config.StrengthY
+
                     MoveMouseRelative(roundToInteger(moveX), roundToInteger(moveY))
-                    Sleep(DelayRate)
+                    Sleep(config.Delay)
                 until not IsMouseButtonPressed(1)
             end
+            Sleep(1)
         until not IsMouseButtonPressed(3)
-    end
-end
-
-function calculateHorizontalRecoil()
-    if HorizontalDirection == "Left" then
-        return -RcCustomStrengthX
-    elseif HorizontalDirection == "Right" then
-        return RcCustomStrengthX
-    else
-        return 0
     end
 end
 
